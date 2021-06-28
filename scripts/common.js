1ef9b536a9e12e1033d88c962001339f53074f2a
tejin3 <86044250+tejin3@users.noreply.github.com>
2021-06-28 14:42:12 +0900

    random

---
+++ scripts/common.js
@@ -0,0 +1,66 @@
+function getAgeGroup(userList, keyAge) {
+    let userAgeGroup = {};
+    //25
+    for (var user of userList) {
+        // var ageGroup = user.age.toString().charAt(0) + "0"; //20
+
+        // //치명적인 문제 4살 - 40대 , 100살 이상 - 10대
+        var ageGroup = "";
+        if (user[keyAge] < 10) {
+            ageGroup = "0";
+        } else if (user[keyAge] >= 100) {
+            ageGroup = user[keyAge].toString().substr(0, 2) + "0";
+
+        } else {
+            ageGroup = user[keyAge].toString().charAt(0);
+        }
+
+
+        if (userAgeGroup[ageGroup]) {
+            userAgeGroup[ageGroup] += 1;
+        } else {
+            userAgeGroup[ageGroup] = 1;
+        }
+    }
+    return userAgeGroup;
+
+}
+// console.log(getAgeGroup());
+
+
+
+
+// 최소값과 최대값 사이의 랜덤한 값 얻는 함수
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+
+function rspPlayer(userRsp) {
+
+    const rsp = ["가위", "바위", "보"];
+
+    // var playRspNum = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+
+
+    // var playRsp = arrRsp[playRspNum];
+
+    const playerRsp = rsp[getRandomInteger(0, 2)];
+
+    const winValue = {
+        "가위": "보",
+        "바위": "가위",
+        "보": "바위"
+    }
+
+    return {
+        "userRsp": userRsp,
+        "playerRsp": playerRsp,
+        "winner": (userRsp == playerRsp) ? 0 : (winValue[userRsp] == playerRsp) ? 1 : -1
+    }
+}
+
+
+console.table(rspPlayer("가위"));
